350c86560b8cda26e45590b73589c6a3793ed283
Fernando Costa Oliveira <fernandolvr49@gmail.com>
2020-07-22 14:43:16 -0300

    Add files via upload

---
+++ Config.php
@@ -0,0 +1,61 @@
+<?php
+
+/**
+ * DATABASE
+ */
+define("CONF_DB_HOST", "localhost");
+define("CONF_DB_USER", "root");
+define("CONF_DB_PASS", "");
+define("CONF_DB_NAME", "");
+
+/**
+ * PROJECT URLs
+ */
+define("CONF_URL_BASE", "");
+define("CONF_URL_ADMIN", CONF_URL_BASE . "/admin");
+define("CONF_URL_ERROR", CONF_URL_BASE . "/404");
+
+/**
+ * DATES
+ */
+define("CONF_DATE_BR", "d/m/Y H:i:s");
+define("CONF_DATE_APP", "Y-m-d H:i:s");
+
+/**
+ * SESSION
+ */
+define("CONF_SES_PATH", __DIR__ . "/../../storage/sessions/");
+
+/**
+ * PASSWORD
+ */
+define("CONF_PASSWD_MIN_LEN", 8);
+define("CONF_PASSWD_MAX_LEN", 40);
+define("CONF_PASSWD_ALGO", PASSWORD_DEFAULT);
+define("CONF_PASSWD_OPTION", ["cost" => 10]);
+
+/**
+ * MESSAGE
+ */
+define("CONF_MESSAGE_CLASS", "trigger");
+define("CONF_MESSAGE_INFO", "info");
+define("CONF_MESSAGE_SUCCESS", "success");
+define("CONF_MESSAGE_WARNING", "warning");
+define("CONF_MESSAGE_ERROR", "error");
+
+/**
+ * MAIL
+ */
+define("CONF_MAIL_HOST", "smtp.sendgrid.net");
+define("CONF_MAIL_PORT", "587");
+define("CONF_MAIL_USER", "apikey");
+define("CONF_MAIL_PASS", "passwd");
+define("CONF_MAIL_SENDER", ["name" => "Fernando C. Oliveira", "address" => "address@hotmail.com"]);
+define("CONF_MAIL_OPTION_LANG", "br");
+define("CONF_MAIL_OPTION_HTML", true);
+define("CONF_MAIL_OPTION_AUTH", true);
+define("CONF_MAIL_OPTION_SECURE", "tls");
+define("CONF_MAIL_OPTION_CHARSET", "urf-8");
+
+
+
